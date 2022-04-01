b02876c2182abaee02b07c80b061e9d0bcfea437
ValeriD <valeri.dobrev10@gmail.com>
2022-04-01 11:05:20 +0300

    WIP tests

--- tests/Hydra/ContractCallTests.cpp
+++ tests/Hydra/ContractCallTests.cpp
@@ -12,26 +12,31 @@
 #include <gtest/gtest.h>
 #include "Hydra/TokenScriptBuilder.h"
 #include "HexCoding.h"
+#include "ComparisonHelper.h"
+#include<vector>
+#include "Bitcoin/TransactionInput.h"
+#include "Bitcoin/Transaction.h"
 
+using namespace TW;
+TEST(Hydra, ContractTokenScript){
 
-TEST(ContractInput, TestInput){
-    auto gasLimit = 1000;
-    auto toAddress = "HPjtxTZwy8i4emXxb64hz82x3s6q48LbGz";
-    auto amount = 10;
-    auto contractAddress = "4ab26aaa1803daa638910d71075c06386e391147";
+    auto res = buildTokenScript(1000, "HPjtxTZwy8i4emXxb64hz82x3s6q48LbGz", 10, "4ab26aaa1803daa638910d71075c06386e391147");
 
-    Hydra::Proto::ContractInput input;
-
-    input.set_gas_limit(gasLimit);
-    input.set_to(toAddress);
-    input.set_amount(amount);
-    input.set_contract_address(contractAddress);
-
-    std::string expected = "";
-    auto res = TW::Hydra::TokenScriptBuilder::buildTokenScript(input);
+    std::string expected = "0x548203e8a9059cbb0000000000000000000000005878623634687a3832783373367134384c62477a000000000000000000000000000000000000000000000000000000000000000aa83461623236616161313830336461613633383931306437313037356330363338366533393131343781c2";
 
     {
-        ASSERT_EQ(hex(res.bytes), expected);
+        ASSERT_EQ(hexEncoded(res.bytes), expected);
     }
+}
+
+TEST(TokenTransaction, TestTransactionBuilder){
+    auto po0 = OutPoint(parse_hex("94be46260220bce2872e3ad69f4b7828792a3db0a6f460a8e956d6ea379b011d"), 1) 
+    auto po1 = OutPoint(parse_hex("7435b2e54bc8e1b0312cc721aaa321df3db59ea721db13188abb8787c1c30257"), 1) 
+    Bitcoin::TransctionInputs utxos;
+    utxos.push_back(Bitcoin::TransactionInput(po0, Bitcoin::Script(), 12545182455)) 
+    utxos.push_back(Bitcoin::TransactionInput(po1, Bitcoin::Script(), 19152684041)) 
+
+    //TODO do not create the objects but use only the 
+
 }
 
