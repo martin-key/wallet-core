39e2638fd563b51d1bc151692ec39fcf389d46e0
ValeriD <valeri.dobrev10@gmail.com>
2022-04-21 14:00:39 +0300

    Package swift update

--- Package.swift
+++ Package.swift
@@ -12,13 +12,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WalletCore",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/2.6.36/WalletCore.xcframework.zip",
-            checksum: "f8bad789644651f7153e5cf46bb3509fb457a14d35301f97863677235c2e602c"
+            url: "https://github.com/ValeriD/wallet-core-1/releases/download/2.6.37/WalletCore.xcframework.zip",
+            checksum: "60bf95a8bf265fa34a2aa52c7768fb3df1cb08c43aef9a81d4cef31c32cdbd61"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/2.6.36/SwiftProtobuf.xcframework.zip",
-            checksum: "40518a795e841bbd2c7e1f7019357b8e09b321a9c1ea264b0819f85575ac04c2"
+            url: "https://github.com/ValeriD/wallet-core-1/releases/download/2.6.37/SwiftProtobuf.xcframework.zip",
+            checksum: "aba1c7639f39d76caaa40ae997d5b488053f0b3f6c7cc1eb4e85a9d4429e79dc"
         )
     ]
 )
