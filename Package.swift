84d0319f2fe6b20dfa2e91b9a33259b7ae7afb12
Valeri Dobrev <valeri.dobrev10@gmail.com>
2022-05-16 17:06:45 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,19 +6,21 @@ let package = Package(
     platforms: [.iOS(.v13)],
     products: [
         .library(name: "WalletCore", targets: ["WalletCore"]),
-        .library(name: "SwiftProtobuf", targets: ["SwiftProtobuf"])
+        .library(name: "SwiftProtobuf", dependencies: ["SwiftProtobuf"])
     ],
-    dependencies: [],
+    dependencies: [
+        .binaryTarget(
+            name: "SwiftProtobuf",
+            url: "https://github.com/ValeriD/wallet-core-1/releases/download/2.6.38/SwiftProtobuf.xcframework.zip",
+            checksum: "0c77d77a9a31b82bed4055cc9f03fb1dd930c23b1f0c8645d9aadb377c300bab"
+        )
+    ],
     targets: [
         .binaryTarget(
             name: "WalletCore",
             url: "https://github.com/ValeriD/wallet-core-1/releases/download/2.6.38/WalletCore.xcframework.zip",
             checksum: "6364d70104789f8aa339e8224881493388c8d4f78db0bfa4a1d507864b878e3b"
         ),
-        .binaryTarget(
-            name: "SwiftProtobuf",
-            url: "https://github.com/ValeriD/wallet-core-1/releases/download/2.6.38/SwiftProtobuf.xcframework.zip",
-            checksum: "0c77d77a9a31b82bed4055cc9f03fb1dd930c23b1f0c8645d9aadb377c300bab"
-        )
+        
     ]
 )
