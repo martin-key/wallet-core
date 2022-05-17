d6090a2f271a574ed8b6306898a1cc1030dce5fc
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2022-05-17 17:24:36 +0300

    Testing without protobuf target

--- Package.swift
+++ Package.swift
@@ -5,19 +5,21 @@ let package = Package(
     name: "WalletCore",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: "WalletCore", targets: ["WalletCore", "SwiftProtobuf"])
+        .library(name: "WalletCore", targets: ["WalletCore"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(
+              name: "SwiftProtobuf",
+              url: "https://github.com/apple/swift-protobuf.git",
+              "1.19.0" ..< "2.0.0"
+            )
+    ],
     targets: [
         .binaryTarget(
             name: "WalletCore",
             url: "https://github.com/ValeriD/wallet-core-1/releases/download/2.6.39/WalletCore.xcframework.zip",
             checksum: "4a79312747c64edde6191712c67d91d2fd9323f18b66115d487173f600041e43"
         ),
-        .binaryTarget(
-            name: "SwiftProtobuf",
-            url: "https://github.com/ValeriD/wallet-core-1/releases/download/2.6.39/SwiftProtobuf.xcframework.zip",
-            checksum: "7072a8af78f0dac3228ddb71c7f312128a209c0ce4962817943702291436a5e1"
-        )
+        
     ]
 )
